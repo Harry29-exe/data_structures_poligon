e2805b80469151de782f6753a952c04c3b6da0e2
Kamil Wójcik <wojcikakamil@gmail.com>
2022-03-03 21:15:59 +0100

    stareted implementing insert for rbtree

--- brtree/rb_tree_set.go
+++ brtree/rb_tree_set.go
@@ -2,7 +2,7 @@ package brtree
 
 import . "data_structures_poligon/utils"
 
-type leafColor = uint
+type leafColor = uint8
 
 const (
 	RED leafColor = iota
@@ -16,53 +16,133 @@ type RBTreeSet[T any, C Compare[T]] struct {
 }
 
 type treeNode[T any] struct {
-	value T
-	color leafColor
-	left  *treeNode[T]
-	right *treeNode[T]
+	value  T
+	color  leafColor
+	parent *treeNode[T]
+	left   *treeNode[T]
+	right  *treeNode[T]
 }
 
-func NewPrimitiveSet[T any]() RBTreeSet[T] {
-	return RBTreeSet[T]{
+func newNode[T](value T, parent *treeNode[T]) *treeNode[T] {
+	return &treeNode[T]{value: value, color: RED, parent: parent}
+}
+
+func NewPrimitiveSet[T any]() *RBTreeSet[T] {
+	return &RBTreeSet[T]{
 		root:        nil,
 		blackHeight: uint(0),
 	}
 }
 
 func (tree *RBTreeSet[T]) Insert(value T) {
+	var node *treeNode[T]
 	if tree.blackHeight == 0 {
 		tree.root = &treeNode[T]{
 			color: BLACK,
 			value: value,
 		}
-		tree.blackHeight = 1
-		return
-	}
+		node = tree.root
+	} else {
+		node = tree.root
+		for {
+			i := tree.compare(node.value, value)
+			if i < 0 {
+				if node.left != nil {
+					node = node.left
+					continue
+				}
+				node.left = newNode(value, node)
+				break
 
-	node := tree.root
-	for {
-		i := tree.compare()
-		if i < 0 {
-			if node.left != nil {
-				node = node.left
-				continue
-			}
-			node.left = &treeNode[T]{
-				value: value,
-				color: RED,
-			}
-			break
-
-		} else if i > 0 {
-			if node.right != nil {
+			} else if i > 0 {
+				if node.right != nil {
+					node = node.right
+					continue
+				}
+				node.right = newNode(value, node)
 				node = node.right
-				continue
+				break
+			} else {
+				return
 			}
-			node.right = &treeNode[T]{}
-			break
-		} else {
-			return
 		}
 	}
 
+	tree.balanceAfterInsert(node)
+}
+
+func (tree RBTreeSet[T]) balanceAfterInsert(newNode *treeNode[T]) {
+	if newNode.parent == nil {
+		newNode.color = BLACK
+		tree.blackHeight++
+		return
+	}
+	if newNode.parent.color == BLACK {
+		return
+	}
+
+	uncle := newNode.getUncle()
+	if uncle.color == RED {
+		newNode.parent.color = BLACK
+		uncle.color = BLACK
+		tree.balanceAfterInsert(newNode.parent.parent)
+	} else {
+
+	}
+}
+
+func (tree RBTreeSet[T]) rotate(node *treeNode[T]) {
+	parent := node.parent
+	grandparent := parent.parent
+
+	if parent.left == node {
+		if grandparent.left == parent {
+
+		}
+	}
+}
+
+func rotateLL[T](node, parent, grandparent *treeNode[T]) {
+	grandparent.left = parent.right
+	parent.parent = grandparent.parent
+	grandparent.parent = parent
+
+	//todo potencial bug (it's the same with LR)
+	//pColor := parent.color
+	//parent.color = grandparent.color
+	//grandparent.color = pColor
+	parent.color = BLACK
+	grandparent.color = RED
+}
+
+func rotateLR[T](node, parent, grandparent *treeNode[T]) {
+	grandparent.left = parent.right
+	parent.left = node.left
+	parent.right = node.right
+	node.left = parent
+	node.right = grandparent
+
+	parent.parent = node
+	node.parent = grandparent.parent
+	grandparent.parent = node
+
+	node.color = BLACK
+	grandparent.color = RED
+}
+
+func rotateRR[T](node, parent, grandparent *treeNode[T]) {
+	grandparent.right = parent.left
+	parent.left = grandparent
+
+	parent.parent = grandparent.parent
+
+}
+
+// getUncle this function assumes that node has a parent that is not root
+func (node *treeNode[T]) getUncle() *treeNode[T] {
+	grandfather := node.parent.parent
+	if grandfather.left == node.parent {
+		return grandfather.right
+	}
+	return grandfather.left
 }
--- main.go
+++ main.go
@@ -1,8 +1,55 @@
 package main
 
-import "data_structures_poligon/linked_list"
+import (
+	"data_structures_poligon/linked_list"
+	"time"
+)
 
 func main() {
+	structPointerTest()
+}
+
+type testStruct struct {
+	name string
+	age  uint8
+}
+
+func structPointerTest() {
+	a1 := testStruct{name: "abcde", age: 1}
+	a1p := &a1
+	a2 := testStruct{name: "abcde", age: 1}
+	a3 := testStruct{name: "abcde", age: 2}
+
+	a1p2 := &a1
+	time0 := time.Now()
+	a1_a2 := a1 == a2
+	a1_a2 = a1 == a2
+	a1_a2 = a1 == a2
+	a1_a2 = a1 == a2
+	a1_a2 = a1 == a2
+
+	time_a1_a2 := time.Now()
+	a1_a3 := a1 == a3
+	a1_a3 = a1 == a3
+	a1_a3 = a1 == a3
+	a1_a3 = a1 == a3
+	a1_a3 = a1 == a3
+
+	time_a1_a3 := time.Now()
+	a1_a1p := a1p2 == a1p
+	a1_a1p = a1p2 == a1p
+	a1_a1p = a1p2 == a1p
+	a1_a1p = a1p2 == a1p
+	a1_a1p = a1p2 == a1p
+	time_a1_a1p := time.Now()
+
+	println(time_a1_a2.UnixNano()-time0.UnixNano(),
+		time_a1_a3.UnixNano()-time_a1_a2.UnixNano(),
+		time_a1_a1p.UnixNano()-time_a1_a3.UnixNano())
+	println(a1_a2 && a1_a3 && a1_a1p)
+}
+
+func linkedListTest() {
 	list := linked_list.New[int]()
 
 	zero, err := list.Get(0)
